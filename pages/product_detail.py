87c6a91bd76d017009efe49d6500f660e7d2f830
Sammit Rane <rane.sammit@gmail.com>
2026-07-01 20:38:52 +0530

    Added the hook to capture screenshots on failure and changed assert to expect to implicitly wait for elements to load to avoid flakiness

--- pages/product_detail.py
+++ pages/product_detail.py
@@ -1,12 +1,17 @@
+from playwright.sync_api import expect
+
+
 class ProductDetailPage:
     def __init__(self, page):
         self.page = page
+        self.product_information = page.locator(".product-information")
 
     def verify_product_details_visible(self):
-        assert self.page.locator(".product-information").is_visible()
+        # Verify product information section is visible
+        expect(self.product_information).to_be_visible()
 
-        assert self.page.locator("text=Category: Women > Tops").is_visible()
-        #assert self.page.locator("text=Price").is_visible()
-        assert self.page.locator("text=Availability").is_visible()
-        assert self.page.locator("text=Condition").is_visible()
-        assert self.page.locator("text=Brand:").is_visible()
+        # Verify product details
+        expect(self.product_information).to_contain_text("Category")
+        expect(self.product_information).to_contain_text("Availability")
+        expect(self.product_information).to_contain_text("Condition")
+        expect(self.product_information).to_contain_text("Brand")
